77c75d291489a58a2b1d0e01ceb81aee5f0cf11d
Miguel Manso <miguel.manso@uniplaces.com>
2017-12-09 23:48:47 +0000

    Add docker support

---
+++ docker/php-fpm/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -xe
+
+# Detect the host IP to add to whitelist ips (index.php)
+export DOCKER_BRIDGE_IP=$(ip ro | grep default | cut -d' ' -f 3)
+
+# See https://shippingdocker.com/xdebug/auto-config/
+#echo "xdebug.remote_host="$XDEBUG_HOST >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+
+composer install --prefer-dist --no-progress --no-suggest
+
+exec php-fpm
