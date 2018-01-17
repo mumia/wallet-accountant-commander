e7708079ae37618200c9fa43bff4c11d0a1680d1
Miguel Manso <miguellmanso@gmail.com>
2018-01-17 02:01:11 +0000

    Add profiler bundle to dev/test

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Prooph\Bundle\EventStore\ProophEventStoreBundle::class => ['all' => true],
     Prooph\Bundle\ServiceBus\ProophServiceBusBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
